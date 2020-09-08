999c7b53485006c494bd9ac9d4b6434384df5967
Clorhexidinum <mkubekov@gmail.com>
2020-09-08 20:58:01 +0300

    ВЕрстка хедера и слайдера

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+
+  $(".banner-slider").slick({
+    dots: true,
+    prevArrow: '  <button class="banner-slider__btn banner-slider__btn--prev"><img src="images/arrow-left.svg" alt="слайд влево"></button>',
+    nextArrow: '  <button class="banner-slider__btn banner-slider__btn--next"><img src="images/arrow-right.svg" alt="слайд вправо"></button>',
+    dotsClass: 'banner-slider__dots',
+  });
+
+
+});
