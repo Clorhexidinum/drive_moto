77be425664e231ba7f955ff6b98657b6bfc11c73
Clorhexidinum <mkubekov@gmail.com>
2021-04-29 14:22:14 +0300

    Update Menu.js

--- app/js/Menu.js
+++ app/js/Menu.js
@@ -1,5 +1,18 @@
 new (class Menu {
-  constructor(item) {
+  constructor(item, img) {
     this.item = item;
+    this.img = img;
+  }
+
+  renderCategoriesMenu(template, arr, parrent) {
+    const link = template.content.querySelector("a");
+    const img = template.content.querySelector("img");
+
+    link.innerHTML = this.item;
+    if (img !== null) {
+      img.setAttribute("src", `images/${this.img}`);
+    }
+
+    parrent.append(template.content.cloneNode(true));
   }
 })();
