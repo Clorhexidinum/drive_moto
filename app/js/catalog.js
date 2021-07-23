2b2998a1094b5c97aa6a203d73ec54ac38a069cd
Clorhexidinum <mkubekov@gmail.com>
2021-07-23 16:49:43 +0300

    q

--- app/js/catalog.js
+++ app/js/catalog.js
@@ -4,39 +4,39 @@ const line = document.querySelector(".catalog__btn-line");
 const itemWrapper = document.querySelectorAll(".product-item__wrapper");
 
 grid.addEventListener("click", () => {
-  grid.classList.add("catalog__filter-button--active");
-  line.classList.remove("catalog__filter-button--active");
-  itemWrapper.forEach((elem) => {
-    elem.classList.remove("product-item__wrapper--list");
-  });
+    grid.classList.add("catalog__filter-button--active");
+    line.classList.remove("catalog__filter-button--active");
+    itemWrapper.forEach((elem) => {
+        elem.classList.remove("product-item__wrapper--list");
+    });
 });
 
 line.addEventListener("click", () => {
-  line.classList.add("catalog__filter-button--active");
-  grid.classList.remove("catalog__filter-button--active");
-  itemWrapper.forEach((elem) => {
-    elem.classList.add("product-item__wrapper--list");
-  });
+    line.classList.add("catalog__filter-button--active");
+    grid.classList.remove("catalog__filter-button--active");
+    itemWrapper.forEach((elem) => {
+        elem.classList.add("product-item__wrapper--list");
+    });
 });
 
 // Топдроп в фильтре мобильная весия
 const filterBtn = document.querySelector(".aside-filter__btn");
 
 filterBtn.addEventListener("click", () => {
-  const asideBtn = document.querySelector(".aside-filter");
+    const asideBtn = document.querySelector(".aside-filter");
 
-  asideBtn.classList.toggle("aside-filter__active");
+    asideBtn.classList.toggle("aside-filter__active");
 });
 
 // Топдроп в фильтре
 const asideFilterBtn = document.querySelectorAll(
-  ".aside-filter__item-title-drop"
+    ".aside-filter__item-title-drop"
 );
 
 asideFilterBtn.forEach((elem) => {
-  elem.addEventListener("click", () => {
-    elem.classList.toggle("aside-filter__item-title-drop--active");
-  });
+    elem.addEventListener("click", () => {
+        elem.classList.toggle("aside-filter__item-title-drop--active");
+    });
 });
 
 let a = new Product(goodsList[0]);
@@ -44,6 +44,6 @@ const goodsOut = document.querySelector(".catalog__product-list");
 let cardTemlate = document.querySelector("#product");
 
 for (let i = 0; i < goodsList.length; i++) {
-  let aa = new Product(goodsList[i]);
-  aa.renderCard(goodsOut, cardTemlate);
+    let aa = new Product(goodsList[i]);
+    aa.renderCard(goodsOut, cardTemlate);
 }
